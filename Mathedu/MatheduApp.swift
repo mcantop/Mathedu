745771e745915d79995e03cc7f89eae15867b205
mcantop <maciejcantop@icloud.com>
2022-08-06 01:56:13 +0200

    Initial Commit

---
+++ Mathedu/MatheduApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatheduApp.swift
+//  Mathedu
+//
+//  Created by Maciej on 06/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct MatheduApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
